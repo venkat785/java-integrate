62da3ac13c542d30cd06f4fe15f407f1dd72c360
Asus <venkycatchme.ps@gmail.com>
2022-05-18 16:18:41 +0530

    adding parameters

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# java-integrate
+# java-integrate# 
+
+This action will generate parameters while executing github actions
+
+## Inputs
+
+## const owner = process.argv[2];
+#const repository = process.argv[3];
+#const base = process.argv[4];
+#const head = process.argv[5];
+
+**Required** 
+
+## Outputs
+
+## 
+
+T
+
+## Example usage
+
+uses: actions/hello-world-javascript-action@v1.1
+with:
+  
---
+++ index.js
@@ -0,0 +1,20 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  // `parameters` input defined 
+  in action metadata file
+  const owner = process.argv[2];
+  const repository = process.argv[3];
+  const base = process.argv[4];
+  const head = process.argv[5];
+  const output = 'ouptut + = ' + owner +' '+ repository +' ' + base +' ' + head; 
+  console.log(output );
+  const time = (new Date()).toTimeString();
+  core.setOutput("output", output);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
