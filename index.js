bb2062e81afb6ae6ab88ee8325b45b4a3e494979
Asus <venkycatchme.ps@gmail.com>
2022-05-23 21:48:42 +0530

    addedPR1.3

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ execute().catch((e) => core.setFailed(e.message));
 octokit.rest.pulls.create({
   owner: 'venkat785',
   repository: 'java-integrate',
-  head: 'main',
-  base: 'jsdev',
+  head: 'jsdev',
+  base: 'main',
 });
 
