4682634410cf9eb640ab3da2bd2a4ef24580b6da
Asus <venkycatchme.ps@gmail.com>
2022-06-07 12:17:07 +0530

    added getBranch3

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function execute() {
   const { data: { branch } } = await octokit.repos.getBranch({
     owner: owner,
     repo: repository,
-    branch: 'dev',
+    branch: base,
   });
 
   var sha1 = branch.Commit.Sha();
