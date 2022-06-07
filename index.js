44b7048913cea5c7ea129ac2713c2fbfb5061d67
Asus <venkycatchme.ps@gmail.com>
2022-06-07 18:35:37 +0530

    added getBranch8

--- index.js
+++ index.js
@@ -20,25 +20,52 @@ const head = core.getInput('head');
 
 async function execute() {
   // Acquire the commits between the head and base
-  const { data: { branch } } = await octokit.repos.getBranch({
+  const { data: { commits } } = await octokit.repos.compareCommits({
     owner: owner,
     repo: repository,
-    branch: base,
+    base: base,
+    head: head,
   });
 
-  var sha1 = branch.Commit.Sha();
+  // Process each commit and get the associated PR 
+  const result = await Promise.all(commits.map(async (commit) => {
+    const prs = [];
+    const { data: associatedPulls } = await octokit.repos.listPullRequestsAssociatedWithCommit({
+      owner: owner,
+      repo: repository,
+      commit_sha: commit.sha,
+    });
+    associatedPulls.forEach(associatedPull => {
+      prs.push({
+        url: associatedPull.html_url,
+        number: associatedPull.number
+      });
+    });
+    return {
+      title: commit.commit.message.split('\n')[0],
+      prs,
+    };
+  }));
 
   // Process each PRs details into a single string
-  var out = 'Owner:' + owner + '\nRepository:' + repository + '\nBase:' + base + '\Sha1:' + sha1 + '\n';
+  var out = 'Owner:' + owner + '\nRepository:' + repository + '\nBase:' + base + '\nHead:' + head + '\n';
+
+  result.forEach((resultItem) => {
+    const pr = resultItem.prs[resultItem.prs.length - 1];
+    const url = (pr) ? pr.url : 'No PR';
+    out = out + (`- ${resultItem.title} (${url})\n`)
+  });
 
   // Write to the log
   console.log(out);
   // Return to the github action
   core.setOutput("output", out);
+  octokit.rest.repos.getBranch({
+    owner: "venkat785",
+    repo: "java-integrate",
+    branch: "fiat",
+  });
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  //const payload = JSON.stringify(github.context.payload, undefined, 2)
-  //console.log('The event payload: ${payload}');
 }
 
 execute().catch((e) => core.setFailed(e.message));
---
+++ work.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { Octokit } = require("@octokit/rest");
+
+if (!Octokit) {
+    throw new Error('@octokit/rest is needed');
+}
+
+const octokit = new Octokit({
+    auth: process.env['GITHUB_TOKEN'],
+});
+
+// `parameters` input defined 
+const owner = core.getInput('owner');
+const repository = core.getInput('repository');
+const base = core.getInput('base');
+const head = core.getInput('head');
+
+async function execute() {
+    // Acquire the commits between the head and base
+    const { data: { branch } } = await octokit.repos.getBranch({
+        owner: owner,
+        repo: repository,
+        branch: base,
+    });
+
+    var sha1 = branch.Commit.Sha();
+
+    // Process each PRs details into a single string
+    var out = 'Owner:' + owner + '\nRepository:' + repository + '\nBase:' + base + '\Sha1:' + sha1 + '\n';
+
+    // Write to the log
+    console.log(out);
+    // Return to the github action
+    core.setOutput("output", out);
+
+    // Get the JSON webhook payload for the event that triggered the workflow
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //console.log('The event payload: ${payload}');
+}
+
+execute().catch((e) => core.setFailed(e.message));
