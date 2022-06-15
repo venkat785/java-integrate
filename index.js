3442cad7781c3e07d6c9be371bc12ff9627f8b78
Asus <venkycatchme.ps@gmail.com>
2022-06-15 11:49:35 +0530

    added branch & PR8

--- index.js
+++ index.js
@@ -52,53 +52,52 @@ async function execute() {
 
   // Write to the log
   console.log('\ncommits = ' + commits[0].sha);
-  /*
-  
-    // Process each commit and get the associated PR 
-    const result = await Promise.all(commits.map(async (commit) => {
-      const prs = [];
-      const { data: associatedPulls } = await octokit.repos.listPullRequestsAssociatedWithCommit({
-        owner: owner,
-        repo: repository,
-        commit_sha: commit.sha,
-      });
-      associatedPulls.forEach(associatedPull => {
-        prs.push({
-          url: associatedPull.html_url,
-          number: associatedPull.number
-        });
-      });
-      return {
-        title: commit.commit.message.split('\n')[0],
-        prs,
-      };
-    }));
-  
-    // Process each PRs details into a single string
-    var out = 'Owner:' + owner + '\nRepository:' + repository + '\nBase:' + base + '\nHead:' + head + '\n';
-  
-    result.forEach((resultItem) => {
-      const pr = resultItem.prs[resultItem.prs.length - 1];
-      const url = (pr) ? pr.url : 'No PR';
-      out = out + (`- ${resultItem.title} (${url})\n`)
+
+  // Process each commit and get the associated PR 
+  const result = await Promise.all(commits.map(async (commit) => {
+    const prs = [];
+    const { data: associatedPulls } = await octokit.repos.listPullRequestsAssociatedWithCommit({
+      owner: owner,
+      repo: repository,
+      commit_sha: commit.sha,
     });
-  
-    // Write to the log
-    console.log(out);
-    // Return to the github action
-    core.setOutput("output", out);
-  
-    const { data: pr } = await octokit.rest.pulls.create({
-      owner: 'venkat785',
-      repo: 'java-integrate',
-      base: 'main',
-      head: 'jsdev',
-      title: 'Merge -> ' + head + ' to ' + base,
-      body: head + '\n' + out,
+    associatedPulls.forEach(associatedPull => {
+      prs.push({
+        url: associatedPull.html_url,
+        number: associatedPull.number
+      });
     });
-  
-    // Write to the log
-    console.log('/nPR title = ' + pr.title);
-  }
-  */
-  execute().catch((e) => core.setFailed(e.message));
+    return {
+      title: commit.commit.message.split('\n')[0],
+      prs,
+    };
+  }));
+
+  // Process each PRs details into a single string
+  var out = 'Owner:' + owner + '\nRepository:' + repository + '\nBase:' + base + '\nHead:' + head + '\n';
+
+  result.forEach((resultItem) => {
+    const pr = resultItem.prs[resultItem.prs.length - 1];
+    const url = (pr) ? pr.url : 'No PR';
+    out = out + (`- ${resultItem.title} (${url})\n`)
+  });
+
+  // Write to the log
+  console.log(out);
+  // Return to the github action
+  core.setOutput("output", out);
+
+  const { data: pr } = await octokit.rest.pulls.create({
+    owner: 'venkat785',
+    repo: 'java-integrate',
+    base: 'main',
+    head: 'jsdev',
+    title: 'Merge -> ' + head + ' to ' + base,
+    body: head + '\n' + out,
+  });
+
+  // Write to the log
+  console.log('/nPR title = ' + pr.title);
+}
+
+execute().catch((e) => core.setFailed(e.message));
