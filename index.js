17d610aa53f00769505b22a1327532b670a6e3d9
Asus <venkycatchme.ps@gmail.com>
2022-06-27 16:36:00 +0530

    added branch,tag,branch9

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function execute() {
   const { data: { verification } } = await octokit.rest.git.createTag({
     owner: owner,
     repo: repository,
-    tag: 'v0.0.5',
+    tag: 'v0.0.6',
     message: 'first version',
     object: branch.commit.sha,
     type: 'commit',
@@ -60,7 +60,7 @@ async function execute() {
   await octokit.git.createRef({
     owner: owner,
     repo: repository,
-    ref: 'refs/tags/v0.0.5',
+    ref: 'refs/tags/v0.0.6',
     sha: branch.commit.sha,
   });
 
