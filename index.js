9d106008fc71a993edc39284fcab5b3504b0b768
Asus <venkycatchme.ps@gmail.com>
2022-06-29 14:36:54 +0530

    calling github action5

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function execute() {
   const { data: { verification } } = await octokit.rest.git.createTag({
     owner: owner,
     repo: repository,
-    tag: 'v0.0.9',
+    tag: 'v0.0.10',
     message: 'first version',
     object: branch.commit.sha,
     type: 'commit',
@@ -60,7 +60,7 @@ async function execute() {
   await octokit.git.createRef({
     owner: owner,
     repo: repository,
-    ref: 'refs/tags/v0.0.9',
+    ref: 'refs/tags/v0.0.10',
     sha: branch.commit.sha,
   });
 
